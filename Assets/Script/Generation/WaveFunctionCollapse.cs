2964d86f340660d9a7bac7a641a9d369aabb9cb7
Stephen Xie <coreficent@gmail.com>
2021-04-24 10:02:45 -0400

    Add wave function collapse pseudo code.

---
+++ Assets/Script/Generation/WaveFunctionCollapse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFunctionCollapse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    /*
+     
+    map = graph
+
+    while map.not_fully_collapsed()
+	    collapsible_chain = dequeue
+	    collapsible_chain.push(map.minimum_block)
+	
+	    while collapsible_chain.not_empty()
+		    block <- collapsible_chain
+		    collapsible_chain.push(block.collapse())
+
+    class block::collaps -> collapsed block list
+    {
+	    result = list
+	    for each in north based on rotation
+		    if not have this north socket given tiles in this block
+			    remove
+			    result.add(north)
+
+	    do the same for other directions
+    }
+
+     */
+}
