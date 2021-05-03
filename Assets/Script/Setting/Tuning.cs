d46ac18f24599c8e51160cdd4fc698d4d4b3fc72
Stephen Xie <coreficent@gmail.com>
2021-05-02 23:02:43 -0400

    Update tuning.

--- Assets/Script/Setting/Tuning.cs
+++ Assets/Script/Setting/Tuning.cs
@@ -3,11 +3,11 @@
     using UnityEngine;
     public class Tuning
     {
-        public static readonly float ActionPerSecond = 250.0f;
+        public static readonly float ActionPerSecond = 50.0f;
         public static readonly float StepInterval = 0.01f;
-        public static readonly int Width = 16;
-        public static readonly int Height = 16;
-        public static readonly int Depth = 12;
+        public static readonly int Width = 12;
+        public static readonly int Height = 12;
+        public static readonly int Depth = 8;
         public static readonly ShadowQuality ShadowSetting = ShadowQuality.Disable;
         public static readonly bool InstantRendering = false;
         public static readonly int MaximumRenderCount = 15;
