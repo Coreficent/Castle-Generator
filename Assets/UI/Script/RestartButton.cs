4f5b5ab86250a542b56e25e99884cc67ff99cf99
Stephen Xie <coreficent@gmail.com>
2021-05-05 09:40:44 -0400

    Pull.

---
+++ Assets/UI/Script/RestartButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class RestartButton: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public TextMeshProUGUI restart;
+    public AudioSource Audio;
+
+    void Start()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        Audio.Play();
+        SceneManager.LoadScene("Main");
+        
+
+    }
+}
+
+
