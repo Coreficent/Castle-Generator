55f8848b348dcb99d8b6fb6f3fdcf68f74c9d84c
Stephen Xie <coreficent@gmail.com>
2021-05-05 01:17:45 -0400

    Implement rectifier.

---
+++ Assets/Shading/Rectifier/CospatialAccumulator.cs
@@ -0,0 +1,9 @@
+﻿namespace Coreficent.Shading
+{
+    using UnityEngine;
+    internal class CospatialAccumulator
+    {
+        public Vector3 Position = new Vector3();
+        public Vector3 Normal = new Vector3();
+    }
+}
---
+++ Assets/Shading/Rectifier/CospatialData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Coreficent.Shading
+{
+    internal class CospatialData
+    {
+        public List<int> CospatialIndexBuffer = new List<int>();
+        public List<CospatialAccumulator> Accumulators = new List<CospatialAccumulator>();
+    }
+}
