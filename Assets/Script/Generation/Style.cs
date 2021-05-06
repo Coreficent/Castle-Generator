4fbb6359014a379626c9dbdf6cb154c88ef17d05
Stephen Xie <coreficent@gmail.com>
2021-05-06 07:02:48 -0400

    Pull style.

--- Assets/Script/Generation/Style.cs
+++ Assets/Script/Generation/Style.cs
@@ -41,28 +41,36 @@
         {
             int style = Random.Range(0, 100);
 
-            if (style < 40)
+            if (style < 25)
             {
                 // winter
                 grass.SetColor(shaderColor, new Color(1.0f, 1.0f, 1.0f, 1.0f));
                 leaf.SetColor(shaderColor, new Color(1.0f, 1.0f, 1.0f, 1.0f));
                 roof.SetColor(shaderColor, new Color(1.0f, 1.0f, 1.0f, 1.0f));
-                postProcessingColor = new Color(0.125f, 0.125f, 0.125f, 1.0f);
+                postProcessingColor = new Color(0.52f, 0.52f, 0.51f, 1.0f);
             }
-            else if (style < 80)
+            else if (style < 50)
             {
                 // fall
-                grass.SetColor(shaderColor, new Color(1.00f, 0.55f, 0.10f, 1.0f));
-                leaf.SetColor(shaderColor, new Color(1.00f, 0.55f, 0.10f, 1.0f));
-
-                postProcessingColor = new Color(1.0f, 1.0f, 0.3f, 1.0f);
-
+                grass.SetColor(shaderColor, new Color(0.90f, 0.45f, 0.00f, 0.1f));
+                leaf.SetColor(shaderColor, new Color(0.90f, 0.30f, 0.00f, 1.0f));
+                roof.SetColor(shaderColor, new Color(0.10f, 0.40f, 1.00f, 1.0f));
+                postProcessingColor = new Color(0.50f, 0.50f, 0.50f, 1.0f);
+            }
+            else if (style < 75)
+            {
+                // night
+                grass.SetColor(shaderColor, new Color(0.317f, 0.772f, 0.572f, 1.0f));
+                leaf.SetColor(shaderColor, new Color(0.317f, 0.772f, 0.572f, 1.0f));
+                roof.SetColor(shaderColor, new Color(0.10f, 0.40f, 1.00f, 1.0f));
+                postProcessingColor = new Color(0.00f, 0.30f, 0.90f, 1.0f);
             }
             else
             {
                 // normal
                 grass.SetColor(shaderColor, new Color(0.317f, 0.772f, 0.572f, 1.0f));
                 leaf.SetColor(shaderColor, new Color(0.317f, 0.772f, 0.572f, 1.0f));
+                roof.SetColor(shaderColor, new Color(0.10f, 0.40f, 1.00f, 1.0f));
                 postProcessingColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
             }
         }
